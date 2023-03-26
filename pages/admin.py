cbe89ba5b69d16f051ee5217360e6e1dce0f0820
sanjana-SH <sanjanahanchate77@gmail.com>
2023-03-26 15:12:57 +0530

    complete Team model

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from . models import *
+from django.utils.html import format_html
 # Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+
+    #to display the img in admin db
+    def thumbnail(self,object):
+        return format_html('<img src="{}" width="40px" style="border-radius:50px;"/>'.format(object.photo.url))
+
+    #for changing the name from thumbnail to photo
+    thumbnail.short_description = 'photo'
+
+
+
+    # to display the table in admin db
+    list_display = ('id','thumbnail','first_name','last_name','designation','created_date')
+    list_display_links = ('id','thumbnail','first_name')
+    search_fields = ('first_name','last_name','designation')
+    list_filter = ('designation',)
+
+
+
+
+admin.site.register(Team,TeamAdmin)
